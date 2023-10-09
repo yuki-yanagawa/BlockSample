af6f4579c65df6025b0dd08719d61960e15888d6
yuki-yanagawa <yanagawa.yuki0507@gmail.com>
2023-10-09 21:57:04 +0900

    First Commit

---
+++ src/main/java/blocksample/block/App.java
@@ -0,0 +1,13 @@
+package blocksample.block;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
