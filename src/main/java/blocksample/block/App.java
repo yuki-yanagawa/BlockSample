15a67ef3bbf9fa03da5320f1b7fbc0383d5d8f17
yuki-yanagawa <50794177+yuki-yanagawa@users.noreply.github.com>
2026-03-31 13:33:04 +0900

    Add callFunc method to print a message

--- src/main/java/blocksample/block/App.java
+++ src/main/java/blocksample/block/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        callFunc();
+    }
+
+    private static void callFunc() {
+        System.out.println("Call Func.");
     }
 }
