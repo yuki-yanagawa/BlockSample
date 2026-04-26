c1966d8b2bb6147d78b3e4e28dbbf9351ca50be0
yuki-yanagawa <yanagawa.yuki0507@gmail.com>
2026-04-26 22:47:33 +0900

    TestCommit1

--- src/main/java/blocksample/block/App.java
+++ src/main/java/blocksample/block/App.java
@@ -14,7 +14,7 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println( "Sample Calll!" );
+        System.out.println( "Sample Calll2!" );
         InnerClass inner = new InnerClass();
         inner.callInnerFunc();
         allCalls();
